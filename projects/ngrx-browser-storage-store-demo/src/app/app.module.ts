9bb44eaeed153846d2b8bf2d4de953087d61a7bb
Vladislav Hadzhiyski <vladislav.hadzhiyski@gmail.com>
2020-04-18 22:32:50 +0300

    Navigation sidenav + theming

--- projects/ngrx-browser-storage-store-demo/src/app/app.module.ts
+++ projects/ngrx-browser-storage-store-demo/src/app/app.module.ts
@@ -1,6 +1,9 @@
+import { LayoutModule } from '@angular/cdk/layout';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,9 +19,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    LayoutModule,
 
     MatButtonModule,
+    MatIconModule,
     MatListModule,
+    MatSidenavModule,
     MatToolbarModule,
 
     StoreModule.forRoot({}),
